fc3012950128db8e38efe9d17bf5c05d3680911b
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-07-02 15:49:36 +0100

    Bump version & tweak write_safe

--- src/helpers/StreamHelpers.mjs
+++ src/helpers/StreamHelpers.mjs
@@ -2,6 +2,7 @@
 
 /**
  * Writes data to a stream, automatically waiting for the drain event if asked.
+ * See also write_safe.
  * @param	{stream.Writable}			stream_out	The writable stream to write to.
  * @param	{string|Buffer|Uint8Array}	data		The data to write.
  * @return	{Promise}	A promise that resolves when writing is complete.
@@ -16,7 +17,7 @@ function write_safe(stream_out, data) {
 		};
 		stream_out.on("error", handler_error);
 		
-		if(stream_out.write(data)) {
+		if(typeof data == "string" ? stream_out.write(data, "utf-8") : stream_out.write(data)) {
 			// We're good to go
 			stream_out.off("error", handler_error);
 			resolve();
@@ -37,6 +38,7 @@ function write_safe(stream_out, data) {
  * @param  {stream.Writable} stream            The stream to end.
  * @param  {Buffer|string} [chunk=undefined] Optional. A chunk to write when calling .end().
  * @return {Promise}                   A Promise that resolves when writing is complete.
+ * @private
  */
 function end_safe(stream, chunk = undefined) {
 	return new Promise((resolve, _reject) => {
