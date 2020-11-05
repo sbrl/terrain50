1eb1073e6ed963e592476e9ccb03d84d60dafc6a
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2020-11-05 15:57:41 +0000

    get_version: I thought we'd fixed this....

--- src/get_version.mjs
+++ src/get_version.mjs
@@ -3,6 +3,9 @@
 import fs from 'fs';
 import path from 'path';
 
+// HACK: Make sure __dirname is defined when using es6 modules. I forget where I found this - a PR with a source URL would be great :D
+const __dirname = import.meta.url.slice(7, import.meta.url.lastIndexOf("/"));
+
 /**
  * Fetches the current version of terrain50.
  * @return	{string}	The current version of the terrain50 library.
